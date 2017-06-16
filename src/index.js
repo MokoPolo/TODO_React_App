37830d19b7cbd799fc3702cece55daf0809a06e3
marc.cueto <marc.cueto@intellera.com>
2017-06-16 13:08:36 -0400

    Trying to remove warnings in code. I think this needs stage-2 babel preset somewhere

--- src/index.js
+++ src/index.js
@@ -6,49 +6,47 @@ const title = <div className="jumbotron text-center"><h1>TODOs</h1></div>;
 class Todos extends React.Component {
   constructor(props) {
     super(props);
-    debugger;
     this.state = {
       txtVal: '',
       listofTodos: props.Listoftodos,
     };
   }
 
+  handleChange = (e) => {
+      this.setState({txtVal: e.target.value})
+    };
+                
   render() {
     return (
       <div className="container">
         <div className="row">
-          <div className="col-md-3"></div>
+          <div className="col-md-3"/>
           <div className="col-md-6">
             <div className="form-group">
               <input id="turns" className="form-control" value={this.state.txtVal} 
-                onChange={(e) => {
-                  debugger
-                  this.setState({txtVal: e.target.value})}}
+                onChange={this.handleChange}
                 onKeyPress={(e) => {
                 if (e.key == "Enter") {
-                  debugger;
-
                   var arrayvar = this.state.listofTodos.slice();
                   arrayvar.push(this.state.txtVal);
-                  this.setState({ listofTodos: arrayvar })
-
+                  this.setState({ listofTodos: arrayvar });
                   this.state.txtVal = "";
                 }}
               } placeholder="What needs to be done" />
             </div>
           </div>
-          <div className="col-md-3"></div>
+          <div className="col-md-3"/>
         </div>
         { 
           this.state.listofTodos.map((todoItem, i) => 
           <div className="row">
-            <div className="col-md-3"></div>
+            <div className="col-md-3"/>
             <div className="col-md-6">
                 <div key={i} className="checkbox">
                 <label><input type="checkbox"/>{todoItem}</label>
                 </div>
             </div>
-            <div className="col-md-3"></div>
+            <div className="col-md-3" />
           </div>
         )}
       </div>
