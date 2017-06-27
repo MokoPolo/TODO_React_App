4ddfb97d8e47d1a7958b717d726fb09f9e2b9008
MokoPolo <Gimpage@hotmail.com>
2017-06-27 09:48:14 -0400

    Update index.js

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,6 +3,7 @@ import todos from './todoReducer';
 import * as todoActions from '../actions/todoActions';
 const rootReducer = combineReducers({
     todossss
+    bla
 });
 
 export default rootReducer;
