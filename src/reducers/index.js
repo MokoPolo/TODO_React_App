fad3074c2d3b9ca13cbb6184dc09c80cb40cd3f0
marc.cueto <marc.cueto@intellera.com>
2017-06-27 09:51:25 -0400

    asdf

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,12 +2,7 @@ import {combineReducers} from 'redux';
 import todos from './todoReducer';
 import * as todoActions from '../actions/todoActions';
 const rootReducer = combineReducers({
-<<<<<<< HEAD
     todossssac
-=======
-    todossss
-    bla
->>>>>>> 4ddfb97d8e47d1a7958b717d726fb09f9e2b9008
 });
 
 export default rootReducer;
