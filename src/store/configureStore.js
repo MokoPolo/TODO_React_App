2f52eaa1135ac172bd45e19d274902c1a86dbdbc
marc.cueto <marc.cueto@intellera.com>
2017-06-27 09:58:30 -0400

    asdf

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -4,7 +4,7 @@ import reduxImmutableStateInvariant from 'redux-immutable-state-invariant';
 
 export default function configureStore(initialState) {
     return createStore(
-        rootReducer,fffir lint
+        rootReducer,
         initialState,
         appffflyMiddleware(reduxImmutableStateInvariant())
     );
